4105c98b939f941f1a6f2d7817886973b01940a3
linghong <linghong_chen@hotmail.com>
2017-02-12 22:08:55 -0500

    set up webpack config file for d3 pages.

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack');
+const path=require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require ('html-webpack-plugin');
+
+module.exports={
+	entry: {
+		bundle: './stock.js',
+		vendor: ['jquery', 'd3', 'd3-axis', 'd3-scale', 'd3-zoom']
+	},
+	output: {
+		//must specify an absolouate path, __dirname references to current directory
+		//save the name as "build"
+		path: path.resolve(__dirname, 'dist'),
+		filename: '[name].js'		
+	},
+	module:{
+		rules: [
+			{
+				use: 'babel-loader',
+				test: /\.js$/,
+				exclude: /node_modules/
+			},
+			{
+				test: /\.css$/,			
+				loader: ExtractTextPlugin.extract({
+					fallbackLoader: 'style-loader',
+					loader: 'css-loader'
+				})	
+			}				
+		]		
+	},
+
+	plugins: [
+		new ExtractTextPlugin('style.css'),
+		new webpack.optimize.CommonsChunkPlugin({
+			names: ['vendor', 'manifest']
+		}),
+		new HtmlWebpackPlugin({
+			template: 'index.html'
+		}),
+		new webpack.ProvidePlugin({
+      		jQuery: 'jquery',
+      		$: 'jquery',
+      		jquery: 'jquery'
+    	}),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+		})
+	]
+};
